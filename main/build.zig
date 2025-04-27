6254f5201951d4827163134cf06c0943e7b8c2e0
Matt Wilbur <wilburm@gmail.com>
2025-04-27 16:47:42 -0400

    Updated for zig 0.14 using xtensa

--- main/build.zig
+++ main/build.zig
@@ -1,22 +1,43 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) void {
-    const target = b.standardTargetOptions(.{});
+pub fn build(b: *std.Build) !void {
+
+    // const target = b.resolveTargetQuery(.{
+    //     .cpu_arch = .riscv32,
+    //     .os_tag = .freestanding,
+    //     .abi = .none,
+    //     .cpu_model = .{ .explicit = &std.Target.riscv.cpu.generic_rv32 },
+    //     .cpu_features_add = std.Target.riscv.featureSet(&.{
+    //         .m, .c,
+    //     }),
+    //     .cpu_features_sub = std.Target.riscv.featureSet(&.{}),
+    // });
+
+    const target = b.resolveTargetQuery(.{
+        .cpu_arch = .xtensa,
+        .os_tag = .freestanding,
+        .abi = .none,
+        .cpu_model = .{ .explicit = &std.Target.xtensa.cpu.esp32 },
+        .cpu_features_add = std.Target.xtensa.featureSet(&.{}), // default
+        .cpu_features_sub = std.Target.xtensa.featureSet(&.{}),
+    });
+
     const optimize = b.standardOptimizeOption(.{});
 
     const lib = b.addStaticLibrary(.{
         .name = "zig",
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    if (std.os.getenv("INCLUDE_DIRS")) |include_dirs| {
-        var it = std.mem.tokenize(u8, include_dirs, ";");
-        while (it.next()) |dir| {
-            lib.addIncludePath(.{ .path = dir });
+    const include_dirs = std.process.getEnvVarOwned(b.allocator, "INCLUDE_DIRS") catch "";
+    if (!std.mem.eql(u8, include_dirs, "")) {
+        var it_inc = std.mem.tokenizeAny(u8, include_dirs, ";");
+        while (it_inc.next()) |dir| {
+            lib.addIncludePath(.{ .cwd_relative = dir });
         }
     }
-
+    lib.linkLibC(); // stubs for libc
     b.installArtifact(lib);
 }
--- main/src/main.zig
+++ main/src/main.zig
@@ -1,10 +1,12 @@
 const std = @import("std");
+const builtin = @import("builtin");
 
 const esp = struct {
     usingnamespace @cImport({
         @cInclude("sdkconfig.h");
         @cInclude("esp_err.h");
         @cInclude("esp_log.h");
+        @cInclude("driver/gpio.h");
 
         // As of Zig 0.11 and version 2.5.2 of the led_strip component, we cannot
         // include the header with @cImport because the led_strip_config_t struct in
@@ -12,28 +14,6 @@ const esp = struct {
         // into an opaque type which causes compilation to fail.
         // @cInclude("led_strip.h");
     });
-
-    const LedStrip = opaque {};
-
-    const StripConfig = extern struct {
-        strip_gpio_num: c_int = 0,
-        max_leds: u32 = 0,
-        led_pixel_format: c_int = 0,
-        led_model: c_int = 0,
-        flags: u32 = 0,
-    };
-
-    const RmtConfig = extern struct {
-        clk_src: c_int = 0,
-        resolution_hz: u32 = 0,
-        mem_block_symbols: usize = 0,
-        flags: u32 = 0,
-    };
-
-    extern fn led_strip_set_pixel(strip: *esp.LedStrip, index: u32, red: u32, green: u32, blue: u32) c_int;
-    extern fn led_strip_refresh(strip: *esp.LedStrip) c_int;
-    extern fn led_strip_clear(strip: *esp.LedStrip) c_int;
-    extern fn led_strip_new_rmt_device(led_config: *const StripConfig, rmt_config: *const RmtConfig, ret_strip: **esp.LedStrip) c_int;
 };
 
 const freertos = @cImport({
@@ -41,124 +21,100 @@ const freertos = @cImport({
     @cInclude("freertos/task.h");
 });
 
-const BLINK_GPIO = esp.CONFIG_BLINK_GPIO;
+//const BLINK_GPIO = esp.CONFIG_BLINK_GPIO;
 
-const LedStrip = struct {
-    state: bool = false,
-    handle: *esp.LedStrip = undefined,
+// pub const std_options = struct {
+//     pub fn logFn(comptime message_level: std.log.Level, comptime scope: @Type(.EnumLiteral), comptime format: []const u8, args: anytype) void {
+//         const color = switch (message_level) {
+//             .err => "\x1b[31m", // red
+//             .warn => "\x1b[33m", // yellow
+//             .info => "\x1b[32m", // green
+//             .debug => "",
+//         };
 
-    pub fn init() !LedStrip {
-        const strip_config: esp.StripConfig = .{
-            .strip_gpio_num = BLINK_GPIO,
-            .max_leds = 1,
-        };
+//         const esp_level = switch (message_level) {
+//             .err => esp.ESP_LOG_ERROR,
+//             .warn => esp.ESP_LOG_WARN,
+//             .info => esp.ESP_LOG_INFO,
+//             .debug => esp.ESP_LOG_DEBUG,
+//         };
 
-        const rmt_config: esp.RmtConfig = .{
-            .resolution_hz = 10_000_000,
-        };
+//         const prefix = switch (message_level) {
+//             .err => "E",
+//             .warn => "W",
+//             .info => "I",
+//             .debug => "D",
+//         };
 
-        var self: LedStrip = .{};
-        switch (esp.led_strip_new_rmt_device(&strip_config, &rmt_config, &self.handle)) {
-            esp.ESP_OK => {},
-            else => |e| {
-                std.log.err("init failed: {}", .{e});
-                return error.Other;
-            },
-        }
-
-        try self.clear();
-        return self;
-    }
-
-    pub fn clear(self: LedStrip) !void {
-        switch (esp.led_strip_clear(self.handle)) {
-            esp.ESP_OK => {},
-            else => |e| {
-                std.log.err("clear failed: {}", .{e});
-                return error.Other;
-            },
-        }
-    }
-
-    pub fn setPixel(self: LedStrip, index: u32, red: u32, green: u32, blue: u32) !void {
-        switch (esp.led_strip_set_pixel(self.handle, index, red, green, blue)) {
-            esp.ESP_OK => {},
-            else => |e| {
-                std.log.err("setPixel failed: {}\n", .{e});
-                return error.Other;
-            },
-        }
-    }
-
-    pub fn refresh(self: LedStrip) !void {
-        switch (esp.led_strip_refresh(self.handle)) {
-            esp.ESP_OK => {},
-            else => |e| {
-                std.log.err("refresh failed: {}\n", .{e});
-                return error.Other;
-            },
-        }
-    }
-
-    pub fn enable(self: LedStrip) !void {
-        try self.setPixel(0, 16, 16, 16);
-        try self.refresh();
-    }
-
-    pub fn disable(self: LedStrip) !void {
-        try self.clear();
-    }
-
-    pub fn toggle(self: *LedStrip) !void {
-        if (self.state) {
-            try self.disable();
-        } else {
-            try self.enable();
-        }
-
-        self.state = !self.state;
-    }
-};
-
-pub const std_options = struct {
-    pub fn logFn(comptime message_level: std.log.Level, comptime scope: @Type(.EnumLiteral), comptime format: []const u8, args: anytype) void {
-        const color = switch (message_level) {
-            .err => "\x1b[31m", // red
-            .warn => "\x1b[33m", // yellow
-            .info => "\x1b[32m", // green
-            .debug => "",
-        };
-
-        const esp_level = switch (message_level) {
-            .err => esp.ESP_LOG_ERROR,
-            .warn => esp.ESP_LOG_WARN,
-            .info => esp.ESP_LOG_INFO,
-            .debug => esp.ESP_LOG_DEBUG,
-        };
-
-        const prefix = switch (message_level) {
-            .err => "E",
-            .warn => "W",
-            .info => "I",
-            .debug => "D",
-        };
-
-        const fmt = std.fmt.comptimePrint(color ++ prefix ++ " (%u): {s}\x1b[0m\n", .{format});
-        const timestamp = esp.esp_log_timestamp();
-        @call(.auto, esp.esp_log_write, .{ esp_level, @tagName(scope), fmt, timestamp } ++ args);
-    }
-};
+//         const fmt = std.fmt.comptimePrint(color ++ prefix ++ " (%u): {s}\x1b[0m\n", .{format});
+//         const timestamp = esp.esp_log_timestamp();
+//         @call(.auto, esp.esp_log_write, .{ esp_level, @tagName(scope), fmt, timestamp } ++ args);
+//     }
+// };
 
 export fn app_main() void {
-    var strip = LedStrip.init() catch {
-        @panic("Failed to initialize LED strip");
+    var io_conf = esp.gpio_config_t{
+        .pin_bit_mask = 1 << 2, // (1 << GPIO_NUM_2)
+        .mode = esp.GPIO_MODE_OUTPUT,
+        .pull_up_en = esp.GPIO_PULLUP_DISABLE,
+        .pull_down_en = esp.GPIO_PULLDOWN_DISABLE,
+        .intr_type = esp.GPIO_INTR_DISABLE,
     };
+    _ = esp.gpio_config(&io_conf);
 
     while (true) {
-        std.log.info("Toggling the LED %s!", .{@as([:0]const u8, if (strip.state) "ON" else "OFF").ptr});
-        strip.toggle() catch {
-            @panic("Toggling the LED failed");
-        };
-        freertos.vTaskDelay(esp.CONFIG_BLINK_PERIOD / freertos.portTICK_PERIOD_MS);
+        // 2. Turn ON the LED (active low = write 0)
+        _ = esp.gpio_set_level(2, 0);
+
+        freertos.vTaskDelay(500 / freertos.portTICK_PERIOD_MS);
+
+        // 3. Turn OFF the LED (active low = write 1)
+        _ = esp.gpio_set_level(2, 1);
+
+        freertos.vTaskDelay(500 / freertos.portTICK_PERIOD_MS);
     }
+}
+
+pub const std_options: std.Options = .{
+    .log_level = .info,
+    .logFn = customLogFn,
+};
+
+fn customLogFn(
+    comptime level: std.log.Level,
+    comptime scope: @Type(.enum_literal),
+    comptime format: []const u8,
+    args: anytype,
+) void {
+    const esp_level = switch (level) {
+        .err => esp.ESP_LOG_ERROR,
+        .warn => esp.ESP_LOG_WARN,
+        .info => esp.ESP_LOG_INFO,
+        .debug => esp.ESP_LOG_DEBUG,
+    };
+
+    const timestamp = esp.esp_log_timestamp();
+
+    const color = switch (level) {
+        .err => "\x1b[31m", // red
+        .warn => "\x1b[33m", // yellow
+        .info => "\x1b[32m", // green
+        .debug => "",
+    };
+
+    const prefix = switch (level) {
+        .err => "E",
+        .warn => "W",
+        .info => "I",
+        .debug => "D",
+    };
+
+    const fmt = std.fmt.comptimePrint(color ++ prefix ++ " (%u): {s}\x1b[0m\n", .{format});
+
+    @call(.auto, esp.esp_log_write, .{
+        esp_level,
+        @as([*c]const u8, @tagName(scope)), // still cast tagName
+        fmt, // but just .ptr for format
+        timestamp,
+    } ++ args);
 }
